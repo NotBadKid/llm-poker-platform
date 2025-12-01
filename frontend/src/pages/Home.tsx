cac09df9ba746dd77cce21b41046f8450f019d95
Maylayloo <milosz.l-24@wp.pl>
2025-12-01 22:47:09 +0100

    Added start / home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,40 +1,38 @@
-import { useNavigate } from "react-router-dom";
-import { startGame } from "../services/gameApi.ts";
+import {useNavigate} from "react-router-dom";
+import HomeButton from "../components/home/HomeButton.tsx";
 
 const Home = () => {
     const navigate = useNavigate();
 
-    const handleStartLLMTable = async () => {
-        try {
-            console.log("Inicjowanie gry...");
-
-            await startGame();
-
-            navigate("/llm-poker-platform/game");
-        } catch (error) {
-            console.error("Błąd startu:", error);
-            alert("Nie udało się wystartować stołu. Sprawdź backend.");
-        }
-    };
-
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-slate-900 text-white gap-10">
-            <h1 className="text-6xl font-bold text-green-500">Poker LLM</h1>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-6xl font-bold absolute top-24 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text">Poker LLM</h1>
 
-            <div className="flex flex-col gap-4">
-                <button
-                    onClick={handleStartLLMTable}
-                    className="px-10 py-5 bg-gradient-to-r from-green-600 to-green-800 rounded-2xl text-3xl font-bold shadow-lg hover:scale-105 transition transform"
-                >
-                    Start
-                </button>
+            <div className="flex flex-col gap-4 mt-24">
+                <HomeButton
+                    content="Create Table"
+                    onClick={() => navigate("/llm-poker-platform/create-table")}
+                    className="bg-violet-700"
+                />
+                <HomeButton
+                    content="Join Table"
+                    onClick={() => {
+                    }}
+                    disabled
+                />
+                <HomeButton
+                    content="LLM statistics"
+                    onClick={() => {
+                    }}
+                    disabled
+                />
+                <HomeButton
+                    content="Player statistics"
+                    onClick={() => {
+                    }}
+                    disabled
+                />
 
-                <button
-                    onClick={() => navigate("/settings")}
-                    className="px-10 py-4 bg-slate-700 rounded-2xl text-xl hover:bg-slate-600 transition"
-                >
-                    Settings
-                </button>
             </div>
         </div>
     );
