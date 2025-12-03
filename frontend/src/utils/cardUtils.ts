7a87dcdf3297d4b1dcb0884d538d676e7866ab3f
Maylayloo <milosz.l-24@wp.pl>
2025-12-03 22:34:47 +0100

    path fixed

--- frontend/src/utils/cardUtils.ts
+++ frontend/src/utils/cardUtils.ts
@@ -1,6 +1,6 @@
 export const getCardImage = (card: string | null): string => {
     if (card === null) {
-        return '/cards/back.png'
+        return 'cards/back.png'
     }
 
     return `cards/${card}.png`
